2930e29cc1a09f88fe7b873f992c14fe5059437b
a.iglovsky <a.iglovsky@robo.cash>
2022-09-25 19:35:02 +0700

    logger changed

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/juju/loggo"
+	"github.com/juju/loggo/loggocolor"
+	"os"
+)
 
 func main() {
+
+	const DefaultWriterName = "default"
+	//panic("eee")
 	fmt.Println("hello")
+	writer, err := loggo.RemoveWriter("default")
+	loggo.RegisterWriter("default", writer)
+	loggo.ReplaceDefaultWriter(loggocolor.NewWriter(os.Stderr))
+	logger := loggo.GetLogger("foo.bar")
+	fmt.Println(logger)
+	fmt.Println(loggo.LoggerInfo())
+	fmt.Println(err)
+	logger.Debugf("sds")
 }
