c30e1d6c562a3feede8da9e6fc2d33d521b91a71
a.iglovsky <a.iglovsky@robo.cash>
2022-10-03 22:07:41 +0700

    main changed

--- main.go
+++ main.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/juju/loggo"
-	"github.com/juju/loggo/loggocolor"
+	logrus "github.com/sirupsen/logrus"
+	"log"
 	"os"
+	"time"
 )
 
-func main() {
+func runAnfWait() int {
+	time.Sleep(time.Second * 1)
 
-	const DefaultWriterName = "default"
-	//panic("eee")
-	fmt.Println("hello")
-	writer, err := loggo.RemoveWriter("default")
-	loggo.RegisterWriter("default", writer)
-	loggo.ReplaceDefaultWriter(loggocolor.NewWriter(os.Stderr))
-	logger := loggo.GetLogger("foo.bar")
-	fmt.Println(logger)
-	fmt.Println(loggo.LoggerInfo())
-	fmt.Println(err)
-	logger.Debugf("sds")
+	return 10
+}
+
+func main() {
+	file, err := os.OpenFile("test.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error while opening", err)
+	}
+	logrus.Info("run")
+	log.SetOutput(file)
+
+	for i := 0; i < 5; i++ {
+		a := runAnfWait()
+		log.Println("runAndWaitFinished...")
+		log.Println("result", a)
+	}
+
+	fmt.Println("done")
 }
